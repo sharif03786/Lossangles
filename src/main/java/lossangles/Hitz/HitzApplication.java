5324f863c2a8e2151e3f1af2b90bc76c9acc17aa
sharif03786 <nagoorsharifsirichapala@gmail.com>
2024-11-24 21:24:25 +0530

    first commit

---
+++ src/main/java/lossangles/Hitz/HitzApplication.java
@@ -0,0 +1,13 @@
+package lossangles.Hitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lossangles/Hitz/HitzApplicationTests.java
@@ -0,0 +1,13 @@
+package lossangles.Hitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
