303c2cfef6d2a57c77f72b3482a1696a3ad44dc3
sharif03786 <nagoorsharifsirichapala@gmail.com>
2024-11-25 07:52:08 +0530

    two variables are added into main method

--- src/main/java/lossangles/Hitz/HitzApplication.java
+++ src/main/java/lossangles/Hitz/HitzApplication.java
@@ -1,13 +1,12 @@
 package lossangles.Hitz;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class HitzApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HitzApplication.class, args);
+public class HitzApplication { 
+                       public static void main(String[] args) {
+		              SpringApplication.run(HitzApplication.class, args);
+		               int j = 20;
+		               int k = 30;
+		                System.out.println(+j);
 	}
-
-}
+	}
