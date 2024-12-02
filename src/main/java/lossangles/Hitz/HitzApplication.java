d09900f573404be5c78e9b62bbb265ee5233f255
sharif03786 <nagoorsharifsirichapala@gmail.com>
2024-12-02 16:29:45 +0530

    i done dicision operation for those veraibles

--- src/main/java/lossangles/Hitz/HitzApplication.java
+++ src/main/java/lossangles/Hitz/HitzApplication.java
@@ -7,16 +7,11 @@ public class HitzApplication {
 		              SpringApplication.run(HitzApplication.class, args);
 		               int j = 20;
 		               int k = 30;
-<<<<<<< HEAD
 					   int q = j+k;
-						
-						
-				    System.out.println(+q);
-=======
-			       int q = j+k;
+					   int d = j/k;
 		        //Addition j and k veraibles
 			    System.out.println(+q);
->>>>>>> 61dbeb3ad77cb933053790ca2489e09b6e4b35a8
+
 						
 	}
 	}
