5257464085a13805d493af74ef9df984246b198f
sharif03786 <nagoorsharifsirichapala@gmail.com>
2024-12-02 22:25:55 +0530

    Update HitzApplication.java

--- src/main/java/lossangles/Hitz/HitzApplication.java
+++ src/main/java/lossangles/Hitz/HitzApplication.java
@@ -7,10 +7,12 @@ public class HitzApplication {
 		              SpringApplication.run(HitzApplication.class, args);
 		               int j = 20;
 		               int k = 30;
-					        int q = j+k;
-					        int d = j/k;
+			      int q = j+k;
+			      int d = j/k;
 		        //Addition j and k veraibles
 			    System.out.println(+q);
+			//Division j and k variables
+			    System.out.println(+d);
 						
 	}
 	}
